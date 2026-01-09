f16dbb2ef102711ca1be6746b3417b68d9fe8e0c
arnav <arnavsingla06@gmail.com>
2026-01-09 11:04:44 +0530

    quicksort

--- Assignment1/quicksort.cpp
+++ Assignment1/quicksort.cpp
@@ -12,13 +12,13 @@ int pivotIdx(vector<int>& arr,int p,int r){
             swap(arr[i],arr[j]);
         }
     }
-    swap(arr[r],arr[j+1]);
-    return j;
+    swap(arr[i+1],arr[r]);
+    return i+1;
 }
 
 void quicksort(vector<int>& arr,int p,int r){
-    if(p>r){
-       int pvt = pivotIdx(arr,p,r);
+    if(p<r){
+       int pvt = pivotIdx(arr,p,r);  
        quicksort(arr,p,pvt-1);
        quicksort(arr,pvt+1,r); 
     }
@@ -27,7 +27,7 @@ void quicksort(vector<int>& arr,int p,int r){
 
 int main(){
     vector<int> arr = {4,5,7,0,8,2,1};
-    quicksort(arr,0,arr.size());
+    quicksort(arr,0,arr.size()-1);
     for(auto it : arr){
         cout<<it<<" ";
     }
